ee1c801bfeddf949eed7515a04da02c9f55f7494
pavan <Pavanpk123@gmail.com>
2022-08-17 15:34:06 +0530

    arithamatic operator

---
+++ arithmetic.js
@@ -0,0 +1,35 @@
+// var x=10;
+// var y=20;
+// var z=x+y;
+// console.log(z);
+
+// var x=51;
+// var y=18;
+// var z=x-y;
+// console.log(z);
+
+// var x=19
+// var y=8
+// var z=x*y
+// console.log(z)
+
+// var x=19
+// var y=88
+// var z=x/y
+// console.log(z)
+
+// var x=19
+// var y=88
+// var z=x%y
+// console.log(z)
+
+var x=19;
+y=x++;
+console.log("x:"+x);
+console.log("y:"+y);
+
+
+// y=8;
+// X=--
+// console.log("x:"+x);
+// console.log("y:"+y)
